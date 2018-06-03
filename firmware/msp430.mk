856a2e49efad55d06886666437f01f49cb73d71f
Kevin Zheng <kevinz5000@gmail.com>
2018-06-02 17:23:24 -0700

    Import MSPGCC toolchain

---
+++ firmware/msp430.mk
@@ -0,0 +1,32 @@
+# You MUST override:
+#DEVICE=msp430f5529
+#MAIN=blink
+
+# Override the following, if necessary:
+MSPDEBUG?=mspdebug tilib
+MSP430_TOOLCHAIN?=/usr/local/gcc-msp430-ti-toolchain-5.01.02.00
+MSP430_SUPPORT_DIR?=${MSP430_TOOLCHAIN}/include
+
+# The rest should be fine:
+CC=${MSP430_TOOLCHAIN}/bin/msp430-elf-gcc
+CFLAGS=-mmcu=${DEVICE} -I ${MSP430_SUPPORT_DIR}
+LDFLAGS=-L ${MSP430_SUPPORT_DIR}
+RM=rm -f
+
+all: ${MAIN}
+
+.SUFFIX: s
+
+.c.s:
+	${CC} -S ${CFLAGS} $<
+
+.PHONY: clean prog verify
+
+clean:
+	${RM} ${MAIN}
+
+prog: ${MAIN}
+	${MSPDEBUG} 'prog ${MAIN}'
+
+verify: ${MAIN}
+	${MSPDEBUG} 'verify ${MAIN}'
