365ed61934c43cd4a77d9920cc166144d8b65e20
Kevin Zheng <kevinz5000@gmail.com>
2018-06-19 12:42:23 -0700

    Build RadioBeacon

--- firmware/msp430.mk
+++ firmware/msp430.mk
@@ -10,21 +10,27 @@ MSP430_SUPPORT_DIR?=${MSP430_TOOLCHAIN}/include
 # The rest should be fine:
 CC=${MSP430_TOOLCHAIN}/bin/msp430-elf-gcc
 CXX=${MSP430_TOOLCHAIN}/bin/msp430-elf-g++
-CFLAGS=-mmcu=${DEVICE} -I ${MSP430_SUPPORT_DIR}
-LDFLAGS=-L ${MSP430_SUPPORT_DIR}
-RM=rm -f
+CFLAGS+=-mmcu=${DEVICE} -I ${MSP430_SUPPORT_DIR} -mrelax -flto
+LDFLAGS+=-L ${MSP430_SUPPORT_DIR}
+RM?=rm -f
 
 all: ${MAIN}
 
-.SUFFIX: s
+.SUFFIX: s cpp
 
 .c.s:
 	${CC} -S ${CFLAGS} $<
 
+.c.o:
+	${CC} -c ${CFLAGS} $< -o $@
+
+.cpp.o:
+	${CXX} -c ${CFLAGS} $< -o $@
+
 .PHONY: clean prog verify
 
 clean:
-	${RM} ${MAIN}
+	${RM} ${MAIN} ${OBJS}
 
 prog: ${MAIN}
 	${MSPDEBUG} 'prog ${MAIN}'
