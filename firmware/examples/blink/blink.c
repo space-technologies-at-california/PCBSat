b54be00ee688cd6185135aee841d48370aeda8ee
Kevin Zheng <kevinz5000@gmail.com>
2018-06-30 15:22:45 -0700

    Update blink for new board

--- firmware/examples/blink/blink.c
+++ firmware/examples/blink/blink.c
@@ -1,16 +1,16 @@
 #include <msp430.h>
 
-#define LED BIT6
+#define LED BIT7
 
 int main() {
     WDTCTL = WDTPW | WDTHOLD;
     unsigned long i = 0;
-    P1DIR = LED;
+    P3DIR = LED;
 
     while (1) {
-        P1OUT = LED;
+        P3OUT = LED;
         __delay_cycles(100000);
-        P1OUT = 0;
+        P3OUT = 0;
         __delay_cycles(100000);
     }
 }
