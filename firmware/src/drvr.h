dd90fa55dbb12866382cc8ad64c40aa0d81c2431
Kevin Zheng <kevinz5000@gmail.com>
2018-08-05 14:09:47 -0700

    Document inductor names

--- firmware/src/drvr.h
+++ firmware/src/drvr.h
@@ -1,9 +1,9 @@
 #pragma once
 
 enum Driver {
-    XAXIS = 0,
-    YAXIS = 1,
-    ZAXIS = 2,
+    XAXIS = 0, // EN_3, Spiral inductor
+    YAXIS = 1, // EN_2, Back top
+    ZAXIS = 2, // EN_1, Back left
 };
 
 void drvr_off(void);
