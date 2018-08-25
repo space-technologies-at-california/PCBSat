3b56d3f3e730f61470e18180d2ddaf97f276f620
Kevin Zheng <kevinz5000@gmail.com>
2018-08-25 11:42:37 -0700

    Remove old Engeria support files

--- tools/energia/Sprite_5/Board.mk
+++
@@ -1,9 +0,0 @@
-######################################
-# Board specific
-ARCH = msp430
-MCU = cc430f5135
-MCU_FLAG = -mmcu=$(MCU)
-F_CPU = 1000000L
-FLASH_SIZE = 16384
-UPLOAD_COMMAND = $(MSPDEBUG) $(VERBOSE_UPLOAD) tilib --force-reset "prog build/$(SKETCH_NAME).bin"
-###########
