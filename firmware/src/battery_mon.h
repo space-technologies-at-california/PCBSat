cd88dd0c1ddbd44f6e31d0322bde11bffb4daf3f
Kevin Zheng <kevinz5000@gmail.com>
2018-09-10 11:22:37 -0700

    Clean up header

--- firmware/src/battery_mon.h
+++ firmware/src/battery_mon.h
@@ -1,19 +1,11 @@
-#ifndef _battery_mon
-#define _battery_mon
+#pragma once
 
-#include <msp430.h>
 #include "adc12_a.h"
 
 extern volatile uint16_t batt_voltage;
-
 extern ADC12_A_configureMemoryParam ADC_config;
 
 extern void setup_bat_monitor(void);
-
-extern void start_bat_monitor(void);
-
-extern void stop_bat_monitor(void);
-
 extern void shutdown_bat_monitor(void);
-
-#endif
+extern void start_bat_monitor(void);
+extern void stop_bat_monitor(void);
