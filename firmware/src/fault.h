149f91277bae515fee16a83aaffd0a205fb7621c
Kevin Zheng <kevinz5000@gmail.com>
2018-09-11 19:31:23 -0700

    Fix fault indices

--- firmware/src/fault.h
+++ firmware/src/fault.h
@@ -3,8 +3,8 @@
 enum {
     FAULT_RECENT_POR = 1,
     FAULT_POWER = 2,
-    FAULT_RADIO = 3,
-    FAULT_LSM_SETUP = 4,
+    FAULT_RADIO = 4,
+    FAULT_LSM_SETUP = 8,
 };
 
 extern volatile uint8_t faults;
