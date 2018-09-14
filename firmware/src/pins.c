c9f16001ebb3a50c52f683d8ef8cd3c2bc4def64
Kevin Zheng <kevinz5000@gmail.com>
2018-09-14 00:45:26 -0700

    Fix redundant pin setup

--- firmware/src/pins.c
+++ firmware/src/pins.c
@@ -8,7 +8,7 @@ void setup_pins() {
     P1REN = ~BIT1; // No PU/PD on PGOOD
     P3DIR = 0xFF;
     PJDIR = 0xFF;
-    P3DIR |= BIT7; P3OUT &= ~ BIT7; // Init LED
+    P3OUT &= ~BIT7; // Init LED
    
     // Allow port remapping
     PMAPKEYID = PMAPKEY;
@@ -18,16 +18,13 @@ void setup_pins() {
     P2MAP1 = PM_TA1CCR2A; // pinPWMmap
     P1MAP4 = PM_UCB0SCL; P1MAP3 = PM_UCB0SDA; // pinI2Cmap
 
-#if 0
-// UART
-#define pinUARTmap()      P1MAP6 = PM_UCA0TXD; P1MAP5 = PM_UCA0RXD
-#endif
-
     // Disallow Port mapping
     PMAPKEYID = 0x0;
 
     // Setup up pins
     P1SEL |= BIT3 | BIT4; // pinI2cConfig: Set P1.4 and P1.3 to USCI function
-    P2SEL |= BIT1 | BIT2; // pinPWMconfig
+#ifdef DEBUG
     P1SEL |= BIT6 | BIT5; // pinUARTconfig: Set P1.6 and P1.5 to USCI Mode
+#endif
+    P2SEL |= BIT1 | BIT2; // pinPWMconfig
 }
