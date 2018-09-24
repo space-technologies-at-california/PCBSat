405d7d4e2e16e489a9e91ca9f278550db6c67141
Kevin Zheng <kevinz5000@gmail.com>
2018-09-23 22:52:41 -0700

    Remove unused code

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -224,8 +224,6 @@ int main() {
             counter_debug = 3;
 #endif
         } else if (actuate_precond()) {
-            /* uint8_t axis = pick_torquer(); */
-            /* int8_t power = 50; */
             uint8_t axis;
             int8_t power;
             magnetorquer_out(&axis, &power);
