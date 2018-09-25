2795dfdd2125000b7479c2a2e207c47a20631f33
Travis Brashears <trbrashears@berkeley.edu>
2018-09-25 16:34:07 -0700

    testing - dont fly

--- firmware/test/test.c
+++ firmware/test/test.c
@@ -3,8 +3,8 @@
 #include "calc.h"
 
 int main() {
-    struct vec3_s m_data = {10, 10, 10}; // put fake data here
-    struct vec3_s g_data = {10, 10, 10}; // and here
+    struct vec3_s m_data = {3, 2, 5}; // put fake data here
+    struct vec3_s g_data = {6, 3, 6}; // and here
     uint8_t axis;
     int8_t power;
     magnetorquer_out(m_data, g_data, &axis, &power);
