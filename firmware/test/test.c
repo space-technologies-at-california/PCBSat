c12b63f8399c8a4bfc6e9aa2f667493646777e48
Travis Brashears <trbrashears@berkeley.edu>
2018-09-25 17:19:50 -0700

    added testing

--- firmware/test/test.c
+++ firmware/test/test.c
@@ -3,8 +3,8 @@
 #include "calc.h"
 
 int main() {
-    struct vec3_s m_data = {300, 200, 500}; // put fake data here
-    struct vec3_s g_data = {600, 300, 600}; // and here
+    struct vec3_s m_data = {30, 20, 50}; // put fake data here
+    struct vec3_s g_data = {60, 30, 6}; // and here
     uint8_t axis;
     int8_t power;
     magnetorquer_out(m_data, g_data, &axis, &power);
