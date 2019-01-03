5735f461f15cd6809833c28c90f1be3ee637417d
Malhar Patel <malhar@berkeley.edu>
2019-01-02 19:47:58 -0700

    Added prediction output to compare against truth and save models within model_dir location for use in tensorboard

--- simulations/python/model.py
+++ simulations/python/model.py
@@ -5,7 +5,7 @@ import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 
-def power_preprocess(df):
+def power_model(df):
     X = df.iloc[:,3:9]
     Y = df.iloc[:,10]
 
@@ -18,16 +18,8 @@ def power_preprocess(df):
 
     x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.20, random_state=42)
 
-    #train = pd.DataFrame(x_train, columns=FEATURES).merge(y_train, left_index=True, right_index=True)
-    #test = pd.DataFrame(x_test, columns=FEATURES).merge(y_test, left_index=True, right_index=True)
-
     train = tf.data.Dataset.from_tensor_slices((dict(x_train), y_train))
     test = tf.data.Dataset.from_tensor_slices((dict(x_test), y_test))
-
-    return train, test
-
-
-def power_model(train, test):
 
     def input_train():
         return (train.shuffle(1000).batch(32).repeat().make_one_shot_iterator().get_next())
@@ -35,7 +27,13 @@ def power_model(train, test):
     def input_test():
         return (test.shuffle(1000).batch(32).make_one_shot_iterator().get_next())
 
-    STEPS = 5000
+    def predict_input_fn(x, y, batch_size=32):
+        ds = tf.data.Dataset.from_tensor_slices((dict(x), y))
+        ds = ds.batch(batch_size)
+        return ds.make_one_shot_iterator().get_next()
+
+
+    STEPS = 15000
 
     featcols = [
         tf.feature_column.numeric_column("mag_x"),
@@ -46,15 +44,20 @@ def power_model(train, test):
         tf.feature_column.numeric_column("gyro_z")
     ]
 
-    model = tf.estimator.DNNRegressor(feature_columns=featcols, hidden_units=[3,2], optimizer='Adam')
+    model = tf.estimator.DNNRegressor(feature_columns=featcols, hidden_units=[32, 16], optimizer='Adam', model_dir='/tmp/model/')
 
     model.train(input_fn=input_train, steps=STEPS)
 
     eval_result = model.evaluate(input_fn=input_test)
 
-    average_loss = eval_result["average_loss"]
+    print("\nAverage loss is {}\n".format(eval_result['average_loss']))
 
-    print("Average loss is {}".format(average_loss))
+    predictions = model.predict(input_fn=lambda: predict_input_fn(x_test[0:32], y_test[0:32]))
+
+    for i, el in enumerate(list(predictions)):
+        print("Predicted: {}".format(el))
+        print("Actual: {}\n".format(y_test.iloc[i]))
+
 
 if __name__ == "__main__":
     '''
@@ -65,15 +68,14 @@ if __name__ == "__main__":
     tf.logging.set_verbosity(tf.logging.INFO)
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f','--file', help='File containing timestamp, lat, lon data', required=True)
+    parser.add_argument('-f','--file', help='Dataset containing mag data, gyro data, axis and power', required=True)
     args = vars(parser.parse_args())
     file_name = args['file']
 
     df = pd.read_csv(file_name)
-    #train_x, test_x, train_y, test_y = preprocess(df)
-    #train_model(train_x, train_y, test_x, test_y)
-    train, test = power_preprocess(df)
-    power_model(train, test)
+    power_model(df)
+
+    #Refer to https://www.tensorflow.org/guide/custom_estimators#running_the_abalone_model - when making axis model 
 
 
 
